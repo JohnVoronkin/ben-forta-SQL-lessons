c5c2ed55dc6b1db194ebe85a4a2222dcfe628215
john voronkin <testinglife.j@gmail.com>
2017-01-23 23:20:10 +0300

    -first commit;

---
+++ Ben_Forta_SQL_lesson_2.sql
@@ -0,0 +1,23 @@
+﻿SELECT prod_id, vend_id, prod_name, prod_price
+FROM Products;
+
+/*
+Только уникальные значения (строки) столбца
+*/
+SELECT DISTINCT vend_id
+FROM Products;
+
+/*
+Лимит выборки строк таблицы
+*/
+SELECT vend_id, prod_name
+FROM Products
+LIMIT 4;
+
+/*
+LIMIT - число возвращаемых строк;
+OFFSET - начальная точка извлечения записей (/строк) из таблицы
+*/
+SELECT vend_id, prod_name
+FROM Products
+LIMIT 2 OFFSET 4;
